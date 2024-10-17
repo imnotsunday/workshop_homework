ade6135be86cf1344a16110fe685dbf8f19168fa
sunduay <205nutb.13@gmail.com>
2024-10-17 23:22:38 +0700

    final eiei

--- login_dockernetwork.dockerfile
+++
@@ -1,5 +0,0 @@
-FROM mcr.microsoft.com/mssql/server:2019-latest
-ENV ACCEPT_EULA=Y
-ENV SA_PASSWORD=Login@TuHW
-ENV MSSQL_PID=Express
-EXPOSE 1433
--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,10 @@
-function callApi() {
+function submitLogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const loginData = {};
     loginData.UserName = username;
     loginData.PassWord = password;
     var jsonData = JSON.stringify(loginData);
-    //const apiUrl = 'https://restapi.tu.ac.th/api/v1/auth/Ad/verify'; 
     fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify2', {
          method: 'POST',
         headers: {
@@ -16,7 +15,7 @@ function callApi() {
     })
     .then(response => response.json())
     .then(data => {
-        const resultDiv = document.getElementById('output');
+        const resultDiv = document.getElementById('massage');
         resultDiv.innerHTML = `
             <p><strong>Status :</strong> ${data.status ? 'Success' : 'Failed'}</p>
             <p><strong>Name :</strong> ${data.displayname_en || 'N/A'}</p>
@@ -25,7 +24,7 @@ function callApi() {
     })
     .catch(error => {
         console.error('Error fetching data:', error);
-        const resultDiv = document.getElementById('output');
+        const resultDiv = document.getElementById('massage');
         resultDiv.innerHTML = '<p>Error fetching data. Please try again.</p>';
     });
 }
